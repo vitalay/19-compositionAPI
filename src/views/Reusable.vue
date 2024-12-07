41f4561e78e5dab2ffdce5f772c687077c46e9a8
vitalya.efimov <vitalay.efimov@gmail.com>
2024-12-07 18:21:38 +0500

    перекрасил кнопки

--- src/views/Reusable.vue
+++ src/views/Reusable.vue
@@ -1,5 +1,9 @@
 <template>
-    <h1>Reusable</h1>
+    <div class="card">
+        <h2>Переиспользование</h2>
+        <button class="btn-primary">Показать сообщение</button>
+        <button class="btn">перейти на главную</button>
+    </div>
 </template>
 
 <script>
@@ -7,5 +11,54 @@
 </script>
 
 <style scoped>
+.card {
+  background-color: #fff;
+  border-radius: 5px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  padding: 20px;
+  width: 300px;
+  margin: 20px auto;
+}
+
+h2 {
+  margin-bottom: 20px;
+  text-align: center;
+}
+
+.btn-primary {
+    width: 100%;
+    height: 40px;
+    margin: 10px;
+  background-color: #4caf50;
+  color: #fff;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  margin-right: 10px;
+  transition: background-color 0.3s ease;
+}
+
+.btn-primary:hover {
+  background-color: #df8a13;
+}
+
+.btn {
+    width: 100%;
+    height: 40px;
+    margin: 10px;
+    background-color: #4caf50;
+  color: #fff;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  margin-right: 10px;
+  transition: background-color 0.3s ease;
+}
+
+.btn:hover {
+  background-color: #df8a13;
+}
 
 </style>
