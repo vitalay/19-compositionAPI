717554504ba8fa056e6f2edc43aa9e43c40281c7
vitalya.efimov <vitalay.efimov@gmail.com>
2024-12-07 15:57:53 +0500

    создал компонент

--- src/App.vue
+++ src/App.vue
@@ -3,27 +3,31 @@
   <div class="card">
     <h1>Vue Composition API</h1>
     <small>data, computed, methods, watch </small>
-    <hr>
-    <p>Название: <strong>{{ name }}</strong></p>
-    <p>Версия: <strong>{{ version }} ({{ doubleVersion }})</strong> </p>
     <div class="form-control">
-      <input type="text" ref="textInput" >
+      <input type="text" v-model="ferstName">
+       <!-- <input type="text" ref="textInput" > -->
     </div>
 
     <button class="btn" @click="change">Изменить</button>
   </div>
+
+  <Framvorkinfo :name="name" :version="version"/>
+
  </div>
  
 </template>
 
 <script>
 import { ref, reactive, toRefs, isRef, isReactive, computed, watch } from 'vue'
-
+import Framvorkinfo from './Framvorkinfo.vue';
 export default {
+
+ 
 setup() {
 const name = ref( 'VueJS')
 const version = ref( 100)
 const textInput = ref(null)
+const ferstName = ref('')
 
 
 
@@ -32,37 +36,43 @@ const framework = reactive({
   version: 3
 })
 
-const doubleVersion = computed(() => version.value * 2)
+//const doubleVersion = computed(() => version.value * 2)
 
-watch ([doubleVersion, name], (newValues, oldValues) => {
-  console.log('new version', newValues[0]),
-  console.log('new name', newValues[1]),
-  console.log('old version', oldValues[0]),
-  console.log('old name', oldValues[1])
+// watch ([doubleVersion, name], (newValues, oldValues) => {
+//   console.log('new version', newValues[0]),
+//   console.log('new name', newValues[1]),
+//   console.log('old version', oldValues[0]),
+//   console.log('old name', oldValues[1])
 
-}) 
+// })
 
 function changeInfo() {
 
 name.value = 'Vue JS !'
 version.value = 150
-console.log(textInput.value.value)
+//console.log(textInput.value.value)
 
  //framework.name = 'Vue JS !'
  //framework.version = 42
  }
 
 return {
+  name, 
+  version,
  
-name: name,
-version: version,
+ 
+//name: name,
+//version: version,
 change: changeInfo,
-doubleVersion,
-textInput
+//doubleVersion,
+//textInput,
+ferstName
 }
-}
-}
+},
+components: { Framvorkinfo },
 
+}
+ 
 </script>
 
 <style scoped>
---
+++ src/Framvorkinfo.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="card">
+        <p>Название: <strong>{{ name }}</strong></p>
+        <p>Версия: <strong>{{ version }} ({{ doubleVersion }})</strong> </p>
+    </div>
+</template>
+
+<script>
+
+export default {
+
+    props: [
+        'name', 
+        'version'
+    ],
+    computed: {
+        doubleVersion() {
+            return this.version * 2
+        }
+    }
+
+}
+</script>
